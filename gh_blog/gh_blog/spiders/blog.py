27bb40ee5c5e773c4a4bfdacf91b4029355c3681
dsjack <dsjack89@gmail.com>
2020-01-19 13:52:29 -0800

    Adds additional spiders for the blog

--- gh_blog/gh_blog/spiders/blog.py
+++ gh_blog/gh_blog/spiders/blog.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
+"""
+    Spiders to crawl Github.blog
+    NOTE: MAKE SURE A DOWNLOAD DELAY OF 10 IS ENABLED 
+          AND ITEM PIPELINE IS SET APPROPRIATELY
+"""
 
 
-class BlogSpider(scrapy.Spider):
+class BlogSpiderAll(scrapy.Spider):
+    """
+    Crawls all pages of Github.blog
+    """
     name = "all-blogs"
     start_urls = [
-        "file:///Users/danjack/gh-blog.html",
-        "file:///Users/danjack/gh-blog2.html",
+        "https://github.blog/",
+        "https://github.blog/page/2/"
     ]
 
     def parse(self, response):
@@ -28,31 +36,63 @@ class BlogSpider(scrapy.Spider):
         next_page = response.xpath('//*[contains(concat(" ",'
                                    'normalize-space(@class)," "),'
                                    '" next_page ")]/@href').get()
-        if next_page is None:
-            next_page = response.xpath('//*[contains(concat(" ",'
-                                       'normalize-space(@class)," "),'
-                                       '" all-posts__view-more ")]/@href').get()
-        # print(next_page)
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
 
-
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
-
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
-
-        next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+
+class BlogSpiderFront(scrapy.Spider):
+    """
+    Crawls only the front page of Github.blog
+    """
+    name = "blog-front"
+    start_urls = [
+        "https://github.blog/",
+    ]
+
+    def parse(self, response):
+        articles = response.xpath('////main//section[contains(concat(" ",normalize-space(@class)," ")," all-posts ")]//article[contains(concat(" ",normalize-space(@class)," ")," post-item ")]')
+        for blog in articles:
+            yield {
+                'title': blog.xpath(
+                    'div/h4/a/text()').get().strip('\n').strip('\t').replace(
+                    "\u2019", "'"),
+                'link': blog.xpath('div/h4/a/@href').get(),
+                'date': blog.xpath('div/a/time/@datetime').get(),
+                'author': blog.xpath(
+                    'div/a/p/text()').get().strip('\n').strip('\t'),
+                'authorProfile': blog.xpath(
+                    'div/a[contains(concat(" ",normalize-space(@class)," "),'
+                    '" author-block ")]/@href').get()
+            }
+
+
+class BlogSpiderFrontFive(scrapy.Spider):
+    """
+    Crawls first five pages of Github.blog
+    """
+    name = "blog-front-five"
+    start_urls = [
+        "https://github.blog/",
+        "https://github.blog/page/2/",
+        "https://github.blog/page/3/",
+        "https://github.blog/page/4/",
+        "https://github.blog/page/5/"
+    ]
+
+    def parse(self, response):
+        articles = response.xpath('////main//section[contains(concat(" ",normalize-space(@class)," ")," all-posts ")]//article[contains(concat(" ",normalize-space(@class)," ")," post-item ")]')
+        for blog in articles:
+            yield {
+                'title': blog.xpath(
+                    'div/h4/a/text()').get().strip('\n').strip('\t').replace(
+                    "\u2019", "'"),
+                'link': blog.xpath('div/h4/a/@href').get(),
+                'date': blog.xpath('div/a/time/@datetime').get(),
+                'author': blog.xpath(
+                    'div/a/p/text()').get().strip('\n').strip('\t'),
+                'authorProfile': blog.xpath(
+                    'div/a[contains(concat(" ",normalize-space(@class)," "),'
+                    '" author-block ")]/@href').get()
+            }
