bd36fc66e1ee1aed0278ebebbb9f8f34df02ace6
dsjack <dsjack89@gmail.com>
2020-01-24 12:21:07 -0800

    Adds another pipeline
    this pipeline drops duplicates that are already added to a csv or json file from previous scrapes

--- gh_blog/gh_blog/pipelines.py
+++ gh_blog/gh_blog/pipelines.py
@@ -4,12 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
-
-
-class GhBlogPipeline(object):
-    def process_item(self, item, spider):
-        return item
+import csv, json, pymongo
+from scrapy.exceptions import DropItem
 
 
 class MongoPipeline(object):
@@ -40,3 +36,26 @@ class MongoPipeline(object):
             return item
         else:
             print('Duplicate title found: "{}"'.format(item["title"]))
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.links_seen = self.get_current_blog_links(f_type='csv')
+
+    def process_item(self, item, spider):
+        if item['link'] in self.links_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.links_seen.add(item['link'])
+            return item
+
+    @staticmethod
+    def get_current_blog_links(f_type='json'):
+        if f_type is 'csv':
+            with open('blogs.csv', newline='', encoding='utf-8-sig') as f:
+                return {row['link'] for row in csv.DictReader(f)}
+        else:
+            with open('blogs.json') as f:
+                return {item['link'] for item in json.load(f)}
+
